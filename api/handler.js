eab0458c7eda3758b21fe03313f731cc2264c703
Anubhav Panda <87078752+halalboro@users.noreply.github.com>
2023-12-24 23:34:07 +0530

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -4,20 +4,29 @@ export default async function handler(req, res) {
     try {
       const { time, temperature, humidity, aqi } = req.body;
 
-      // logging it
+      // Logging received data
       console.log('Received data:');
       console.log('Time:', time);
       console.log('Temperature:', temperature);
       console.log('Humidity:', humidity);
       console.log('AQI:', aqi);
 
-      res.status(200).json({ success: true });
+      return res.status(200).json({ success: true });
     } catch (error) {
       console.error('Error processing data:', error);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  } else if (req.method === 'GET') {
+    try {
+      // Respond with a JSON object containing the data
+      return res.status(200).json({ time, temperature, humidity, aqi });
+    } catch (error) {
+      console.error('Error processing data:', error);
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
     }
   } else {
-    res.status(405).json({ success: false, error: 'Method Not Allowed' });
+    return res.status(405).json({ success: false, error: 'Method Not Allowed' });
   }
 }
+
 
